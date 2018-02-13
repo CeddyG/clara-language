c2fac01e36b897bc44727d88c2c6f583b1c2684f
CeddyG <ceddyg@free.fr>
2018-02-13 21:54:52 +0100

    -Fix getActiveLang, get the right traduction

--- src/Repositories/LangRepository.php
+++ src/Repositories/LangRepository.php
@@ -4,11 +4,19 @@ namespace CeddyG\ClaraLanguage\Repositories;
 
 use CeddyG\QueryBuilderRepository\QueryBuilderRepository;
 
+use App;
+
 class LangRepository extends QueryBuilderRepository
 {
+    protected $iIdCurrentLang = 0;
+
     protected $aFillable = [
         'lang_code',
 		'lang_active'
+    ];
+    
+    protected $aRelations = [
+        'text_lang'
     ];
     
     /**
@@ -19,6 +27,9 @@ class LangRepository extends QueryBuilderRepository
     protected $aCustomAttribute = [
         'active_label' => [
             'lang_active'
+        ],
+        'name' => [
+            'traduction_lang.lang_name'
         ]
     ];
     
@@ -26,6 +37,10 @@ class LangRepository extends QueryBuilderRepository
     {
         $this->sTable       = config('clara-lang.table', 'lang');
         $this->sPrimaryKey  = config('clara-lang.primary_key', 'id_lang');
+        
+        $this->iIdCurrentLang = (int) $this->findByField('lang_code', App::getLocale(), ['id_lang'])
+            ->first()
+            ->id_lang;
     }
     
     protected function getActiveLabelAttribute($oItem)
@@ -33,18 +48,33 @@ class LangRepository extends QueryBuilderRepository
         return $oItem->lang_active == 0 ? __('clara-lang.no') : __('clara-lang.yes');
     }
     
+    protected function getNameAttribute($oItem)
+    {
+        return $oItem->text_lang->first()->lang_name;
+    }
+    
+    public function traduction_lang()
+    {
+        return $this->hasMany('CeddyG\ClaraLanguage\Repositories\TextLangRepository', 'fk_lang', [['fk_traduction', '=', $this->iIdCurrentLang]]);
+    }
+    
     public function text_lang()
     {
         return $this->hasMany('CeddyG\ClaraLanguage\Repositories\TextLangRepository', 'fk_lang');
     }
 
+    public function getIdCurrentLang()
+    {
+        return $this->iIdCurrentLang;
+    }
+    
     public function getActiveLang()
     {
         return $this->findWhere(
             [
-                ['lang_active', '=', 1],
-                ['text_lang.fk_lang', '=', 1]
+                ['lang_active', '=', 1]                
             ], 
-            ['id_lang', 'text_lang.lang_name']);
+            ['id_lang', 'name']
+        );
     }
 }
